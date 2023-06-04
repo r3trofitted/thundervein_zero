c4bf1ca2c6ba2de0f12a9d22b31257eeceff2f65
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-06-04 11:01:44 +0200

    Move resolution by a `Turn` object
    It is very rough at the moment, but it works. Move orders are applied
    to the new board, by replacing the (immutable) `Zone` objects with new ones,
    based on the move's specifications.

--- app/models/turn.rb
+++ app/models/turn.rb
@@ -6,8 +6,19 @@ class Turn < ApplicationRecord
   
   def resolve!
     new_turn = dup.tap { |t| t.increment :number }
+    moves.each do |move|
+      origin = Board::Zone.new occupant: move.player, units: move.origin.units - move.units
+      target = Board::Zone.new occupant: move.player, units: move.target.units + move.units
+      
+      new_turn.board.public_send "#{move.origin_before_type_cast}=", origin
+      new_turn.board.public_send "#{move.target_before_type_cast}=", target
+    end
     new_turn.save!
     yield new_turn if block_given?
     true
+  end
+  
+  def moves
+    orders.where(type: "Move")
   end
 end
--- test/models/turn_test.rb
+++ test/models/turn_test.rb
@@ -15,4 +15,24 @@ class TurnTest < ActiveSupport::TestCase
       assert_equal new_turn.board.west, turn.board.west
     end
   end
+  
+  test "resolving a turn with move orders" do
+    turn = @ongoing_game_turn_3
+    
+    # At the beginning of turn 3, Karima has 3 in South and 5 units in East
+    south, east = turn.board.south, turn.board.east
+    assert_equal @karima, south.occupant
+    assert_equal 3, south.units
+    assert_equal @karima, east.occupant
+    assert_equal 5, east.units
+    
+    turn.orders << Move.new(player: @karima, origin: :south, target: :east, units: 2)
+    turn.resolve! do |new_turn|
+      south, east = new_turn.board.south, new_turn.board.east
+      assert_equal @karima, south.occupant
+      assert_equal 1, south.units
+      assert_equal @karima, east.occupant
+      assert_equal 7, east.units
+    end
+  end
 end
