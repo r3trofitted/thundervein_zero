544e1626ef195c8a0379be230c5614b892f24fce
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-06-04 09:04:05 +0200

    Refactored the Attack a bit
    The validators have been moved to the parent class Order (even when they
    are specific to a single type of Order, this seemed cleaner), as have
    the common validation.
    
    Note, however, that Order being an kind-of abstract class (it's not one
    in the Rails STI [`abstract_class`](http://api.rubyonrails.org/classes/ActiveRecord/Inheritance/ClassMethods.html#abstract_class)
    meaning, but that's another story), I've kept the tests duplicated in
    the `MoveTest` and `AttackTest` suites.

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,32 @@ class Order < ApplicationRecord
   
   delegate :board, to: :turn
   
+  class OccupiedByPlayerValidator < ActiveModel::EachValidator
+    def validate_each(record, attribute, value)
+      record.errors.add(attribute, :not_occupied_by_player) unless value.occupant == record.player
+    end
+  end
+  
+  class EmptyOrOccupiedByPlayerValidator < ActiveModel::EachValidator
+    def validate_each(record, attribute, value)
+      if occupant = value.occupant
+        record.errors.add(attribute, :occupied) unless occupant == record.player
+      end
+    end
+  end
+  
+  class OccupiedByAnotherPlayerValidator < ActiveModel::EachValidator
+    def validate_each(record, attribute, value)
+      if occupant = value.occupant
+        record.errors.add(attribute, :occupied_by_player) if occupant == record.player
+      else
+        record.errors.add(attribute, :empty)
+      end
+    end
+  end
+  
+  validates :origin, presence: true, occupied_by_player: true
+
   def origin
     board.public_send super
   end
