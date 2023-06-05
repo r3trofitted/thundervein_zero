df956991db44bf298abf3f9d249f1d78e3c86542
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-06-05 16:31:18 +0200

    Resolution of turn with pending attacks

--- app/models/turn.rb
+++ app/models/turn.rb
@@ -14,11 +14,15 @@ class Turn < ApplicationRecord
   enum :status, %i(started resolution_in_progress finished), default: :started
   
   def resolve!
-    new_turn = dup.tap { |t| t.increment :number }
+    resolution_in_progress!
+    
+    return false if attacks.any? &:pending?
     
     # less efficient that +.update_all+, but perf is not an issue yet, so 
     # let's keep the callbacks and have the timestamps be taken care of along the status
     orders.colliding.each &:canceled!
+    
+    new_turn = dup.tap { |t| t.increment :number }
     
     moves.to_carry_out.each do |move|
       new_turn.board.move(move.units, from: move.origin, to: move.target)
@@ -34,5 +38,9 @@ class Turn < ApplicationRecord
   
   def moves
     orders.where(type: "Move")
+  end
+  
+  def attacks
+    orders.where(type: "Attack")
   end
 end
