5a03f541646dd4602171a359e531138d1ef79cfb
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-06-05 13:10:19 +0200

    Fixed the `Order.colliding` bug
    The previous implementation was too simple, and buggy: an order could
    be counted as colliding if it has the same target as with _any_
    colliding order, even in a different turn.
    
    (That will teach me not to test scopes…)
    
    Not that I'm a bit on the fence on this solution. Not so much because
    of the possible performance issue (as said in the code comments: we're
    not there yet), but because it required to write raw SQL and move away
    from the nice abstractions of ActiveRecord. I don't know if it's a legit
    "power user" case, or just that I don't know AR well enough to enjoy its
    "conceptual compression". I basically wrote the SQL that I wanted, and
    then backtracked to have as many AR stuff as possible.
    
    Also note that I tried to use NOT EXISTS instead of a the "LEFT JOIN +
    IS NULL" combo, but couldn't make it work. (Obviously, I'm in the
    demographics for conceptual compression.)
    
    As a bonus, I cleaned up the code a bit and introduced my frivolous
    darling, `Module.concerning`.

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,36 +2,83 @@ class Order < ApplicationRecord
   belongs_to :turn
   belongs_to :player
   
-  scope :colliding, -> { where(target: Order.select(:target).group(:turn_id, :target).having("COUNT(target) > 1")) }
-  scope :not_colliding, -> { where(target: Order.select(:target).group(:turn_id, :target).having("COUNT(target) = 1")) }
-  
   enum :status, %i(received carried_out canceled), default: :received
   
   delegate :board, to: :turn
   
-  class OccupiedByPlayerValidator < ActiveModel::EachValidator
-    def validate_each(record, attribute, value)
-      record.errors.add(attribute, :not_occupied_by_player) unless record.board.occupant_of(value) == record.player
+  concerning :Scopes do
+    included do
+      # Returns all the orders for which there is a collision.
+      #
+      # (A _collision_ happens when, in the same turn, two orders have the same
+      # target.)
+      #
+      # OPTIMIZE: using a subquery for _all_ the collisions could eventually 
+      # cause performance issues, but we're not there yet. In the meantime, this 
+      # is (hopefully) easy to understand and DB-independent.
+      scope :colliding, -> { 
+        joins(<<~SQL)
+          INNER JOIN (#{Order.grouped_by_collisions.to_sql}) AS collisions
+          ON orders.turn_id = collisions.turn_id AND orders.target = collisions.target
+        SQL
+      }
+      
+      # Returns all the orders for which there is *no* collision.
+      #
+      #
+      # (A _collision_ happens when, in the same turn, two orders have the same
+      # target.)
+      #
+      # OPTIMIZE: using a subquery for _all_ the collisions could eventually 
+      # cause performance issues, but we're not there yet. In the meantime, this 
+      # is (hopefully) easy to understand and DB-independent.
+      scope :not_colliding, -> {
+        joins(<<~SQL)
+          LEFT JOIN (#{Order.grouped_by_collisions.to_sql}) AS collisions
+          ON orders.turn_id = collisions.turn_id AND orders.target = collisions.target
+        SQL
+        .where("collisions.turn_id IS NULL")
+      }
+      
+      # Two orders with the same target, in the same turn, cause a _collision_.
+      #
+      # Warning: this scope only retrieves the +:turn_id+ and +:target+ fields, 
+      # and because the results are grouped, not _all_ colliding orders with 
+      # be returned. Still useful to know which turns and/or zones are involved
+      # in collisions.
+      scope :grouped_by_collisions, -> {
+        select(:turn_id, :target).group(:turn_id, :target).having("COUNT(target) > 1")
+      }
     end
   end
   
-  class EmptyOrOccupiedByPlayerValidator < ActiveModel::EachValidator
-    def validate_each(record, attribute, value)
-      if occupant = record.board.occupant_of(value)
-        record.errors.add(attribute, :occupied) unless occupant == record.player
+  concerning :Validations do
+    class OccupiedByPlayerValidator < ActiveModel::EachValidator
+      def validate_each(record, attribute, value)
+        record.errors.add(attribute, :not_occupied_by_player) unless record.board.occupant_of(value) == record.player
       end
     end
-  end
-  
-  class OccupiedByAnotherPlayerValidator < ActiveModel::EachValidator
-    def validate_each(record, attribute, value)
-      if occupant = record.board.occupant_of(value)
-        record.errors.add(attribute, :occupied_by_player) if occupant == record.player
-      else
-        record.errors.add(attribute, :empty)
+    
+    class EmptyOrOccupiedByPlayerValidator < ActiveModel::EachValidator
+      def validate_each(record, attribute, value)
+        if occupant = record.board.occupant_of(value)
+          record.errors.add(attribute, :occupied) unless occupant == record.player
+        end
       end
     end
+    
+    class OccupiedByAnotherPlayerValidator < ActiveModel::EachValidator
+      def validate_each(record, attribute, value)
+        if occupant = record.board.occupant_of(value)
+          record.errors.add(attribute, :occupied_by_player) if occupant == record.player
+        else
+          record.errors.add(attribute, :empty)
+        end
+      end
+    end
+    
+    included do
+      validates :origin, presence: true, occupied_by_player: true      
+    end
   end
-  
-  validates :origin, presence: true, occupied_by_player: true
 end
