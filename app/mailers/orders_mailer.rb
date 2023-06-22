c84daaa97bf6d77e3a412675190e281f4b6297ac
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-06-22 11:48:48 +0200

    Removed all use of i18n
    Let's try _not_ to use it this time – since most texts will eventually
    be machine-generated, we don't gain much by paying for the extra
    indirection that translation files bring.

--- app/mailers/orders_mailer.rb
+++ app/mailers/orders_mailer.rb
@@ -2,10 +2,6 @@ class OrdersMailer < ApplicationMailer
   before_action { @order = params[:order] }
 
   def confirmation
-    mail(
-      to: @order.player_email_address,
-      from: "orders@#{@order.game_id}.thundervein-0.game",
-      subject: default_i18n_subject(game_id: @order.game_id, turn_number: @order.turn_number)
-    )
+    mail subject: "Order received (game ##{@order.game_id}, turn #{@order.turn_number})"
   end
 end
