3a2b40c92163c12ae5c61c817dfed41ea391e824
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-06-22 14:06:56 +0200

    Added mailer preview for `participation_confirmed`

--- test/mailers/previews/participations_mailer_preview.rb
+++ test/mailers/previews/participations_mailer_preview.rb
@@ -1,5 +1,5 @@
 class ParticipationsMailerPreview < ActionMailer::Preview
   def participation_confirmed
-    # TODO
+    ParticipationsMailer.with(participation: Participation.first).participation_confirmed
   end
 end
