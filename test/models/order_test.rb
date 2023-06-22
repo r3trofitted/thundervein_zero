aadb9318386210f386ae9495557ae3c5e532dad7
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-06-22 12:07:32 +0200

    Move the "order from text" back to the mailbox
    We will eventually need an object to handle the NLP that will turn an
    incoming email into a new `Order` object. However, at the moment, I find
    this responsibility better handed off to the `CommandsMailbox`. It is
    definitely not SRP-compliant, but since the mailbox already prepares
    the resources for other commands (such as the `Participation` if the
    incoming email is a request to join a game), let's at least have a
    coherent architecture – even if it makes the mailbox object do too
    many things for a while.

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,23 +1,6 @@
 require "test_helper"
 
 class OrderTest < ActiveSupport::TestCase
-  test "building an Order from a text" do
-    order = Order.from_text "Move 2 units from North to South"
-    
-    assert_instance_of Move, order
-    assert_equal 2, order.units
-    assert_equal "north", order.origin
-    assert_equal "south", order.target
-    
-    # Capitalization is not an issue
-    order = Order.from_text "attack with 3 Units from west to South"
-    
-    assert_instance_of Attack, order
-    assert_equal 3, order.units
-    assert_equal "west", order.origin
-    assert_equal "south", order.target
-  end
-  
   test "invalid if the player is not a participant in the related game" do
     order = Order.new turn: @new_game.current_turn, player: @eisha
     
